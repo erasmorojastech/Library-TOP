fc88506ea8a41b823a413fec1c68a705d18c2984
erasmorojastech <erasmorojastech@gmail.com>
2023-12-12 12:00:00 -0300

    Add function to render books from array on the page

--- scripts/index.js
+++ scripts/index.js
@@ -17,3 +17,24 @@ const addBookToLibrary = function () {
 
     myLibrary.push(newBook);
 }
+
+const renderLibraryCards = function () {
+    const library = document.querySelector("#library")
+
+    const myLibraryCards = myLibrary.map((e) => {
+        return (`<div class="book-card">
+            <h2>${e.title}</h2>
+            <h3>${e.author}</h3>
+            <p>${e.pages}</p>
+            <p>${e.read}</p>
+        </div>`)
+    });
+
+    library.innerHTML = "";
+    myLibraryCards.forEach((e, i) => {
+        const newChild = document.createElement('div');
+        newChild.setAttribute("class", "book-card")
+        newChild.innerHTML = e;
+        library.appendChild(newChild);
+    })
+}
